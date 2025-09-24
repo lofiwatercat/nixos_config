153259a811d0cf161691d52f0dd9653117407816
lofiWaterCat <alan001tk@gmail.com>
2025-09-23 20:35:43 -0700

    Add gamescope

--- hosts/options/steam.nix
+++ hosts/options/steam.nix
@@ -6,6 +6,11 @@
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
     # localNetworkGameTransfers.openFirewall = true;
+  };
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
   };
 }
